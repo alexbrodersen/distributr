e96994ee752b6ef9885e7e9a73e8592a32068d51
patr1ckm <patrick.mil10@gmail.com>
2016-06-04 08:43:36 -0400

    first commit of distributr materials

---
+++ tests/testthat/test_summary_gapply.R
@@ -0,0 +1,33 @@
+context("test_summary_gapply")
+
+
+do.one <- function(a=1,b=2){data.frame(sum=a+b,sub=a-b)}
+out <- gapply(do.one,.reps=3, a=1,b=2,.verbose=0)
+x <- capture.output(o <- summary(out))
+expect_output(summary(out), "Source:")
+expect_output(summary(out), "Estimated time")
+expect_output(summary(out), "Number of conditions: ")
+x <- summary(out)
+expect_is(x,"tbl")
+expect_equal(dim(x), c(2,4))
+
+do.one <- function(a=1,b=2,d=4){data.frame(sum=a+b+rnorm(1,d),sub=a-b)}
+out <- gapply(do.one,.reps=3, a=1:3,b=2,d=4:5, .verbose=0)
+
+
+## Tests of .fun
+#expect_equivalent(as.numeric(summary(out, .fun=max)[1,".fun(value)"]), max(out[1:3,"value"]))
+#expect_equivalent(mean(out[1:3,"value"]), as.numeric(summary(out)[1,".fun(value)"]))
+
+## Tests of .key
+expect_true(all(summary(out, .key="sum")$key == "sum"))
+expect_true(all(summary(out, .key="sub")$key == "sub"))
+
+
+## Plots
+do.one <- function(a=1,b=2,d=4){data.frame(sum=a+b+rnorm(1,d),sub=a-b)}
+out <- gapply(do.one,.reps=3, a=1:3,b=2,d=4:5, .verbose=0)
+
+do.one <- function(a=1){data.frame(sum=a+a, sub=a-a)}
+out <- gapply(do.one, .reps=3, a=1:3)
+plot(out)
