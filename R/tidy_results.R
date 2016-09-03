70524a314c570a09cf8a2947e5fdd73d1488503e
patr1ckm <patrick.mil10@gmail.com>
2016-09-03 11:23:38 -0400

    first commit of new (awesomely faster) tidy.gresults

---
+++ R/tidy_results.R
@@ -0,0 +1,84 @@
+tidy.gresults <- function(x, arg_grid=NULL, .reps=NULL){
+
+  if(is.null(arg_grid)){ arg_grid <- attr(x, "arg.grid")}
+  if(is.null(.reps)){ .reps = attr(x, ".reps")}
+
+  rep_grid <- arg_grid[rep(1:nrow(arg_grid),each=.reps), , drop=F]
+  rep_grid$rep  <- rep(1:.reps, times=nrow(arg_grid))
+
+  # Stack results, adding keys according to names of elements, colnames, and rownames.
+  values <- stack_list(x)
+
+  # Expand rows by number of keys
+  if(is.data.frame(x[[1]])){
+    nkeys <- sapply(x, function(xi){prod(dim(xi))})
+  } else {
+    nkeys <- sapply(x, length)
+  }
+
+  value_grid <- rep_grid[rep(1:nrow(rep_grid), nkeys), ]
+
+  rownames(value_grid) <- NULL
+  res <- dplyr::as_data_frame(value_grid) %>% dplyr::bind_cols(., dplyr::bind_rows(val))
+
+  new.attr.names <- setdiff(names(attributes(x)), names(attributes(res)))
+  attributes(res)[new.attr.names] <- attributes(x)[new.attr.names]
+  attr(res, "class") <- c("gresults", class(res))
+  return(res)
+}
+
+#' Stacks a list of simple vectors or data frames using names of elements, columns, or rows as keys.
+stack_list <- function(xl){
+  x <- xl[[1]]
+  if(is.data.frame(x)){
+    same_dimensions <- all(sapply(lapply(xl, dim), function(dims){ identical(dims, dim(x))}))
+
+    if(same_dimensions){
+      value <- xl %>% bind_rows %>% gather
+      if(nrow(x) > 1){
+        value <- value %>% transmute(key = key,
+                                     key2 = rep(rownames(x), times = length(xl) * ncol(x)),
+                                     value = value)
+      }
+    } else {
+      value <- lapply(xl, stack_x) %>% bind_rows
+    }
+
+  } else {
+    same_dimensions <- all(sapply(xl, length) == length(xl[[1]]))
+
+    if(same_dimensions){
+      value <- do.call(rbind, xl) %>% as.data.frame %>% gather
+    } else{
+      value <- lapply(xl, stack_x) %>% bind_rows
+    }
+  }
+  return(value)
+}
+
+#' Stack a simple vector or data frame into key and value columns
+#'
+#' Stacks a vector into 'key' and 'value' columns, where key takes the names of the elements, or
+#' assigns names if null.
+#'
+#' Stacks a data frame into 'key' (key2) and 'value' columns, where key and key2 are the column
+#' and row names of x.
+#'
+#' @param x a vector or data frame
+stack_x <- function(x){
+  if(is.data.frame(x)){
+    if(nrow(x) > 1){
+      data_frame(key = rep(colnames(x), each = nrow(x)),
+                 key2 = rep(rownames(x), times = ncol(x)),
+                 value = unlist(x, use.names = F))
+    } else {
+      data_frame(key = names(x), value = unlist(x, use.names = F))
+    }
+  } else {
+    if(is.null(names(x))){
+      data_frame(key = paste0("V", seq_along(x)), value = x)
+    } else {
+      data_frame(key = names(x), value = x)
+    }
+  }
+}
