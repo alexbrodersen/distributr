689726d50a63970f409858618319318b6d821f18
patr1ckm <patrick.mil10@gmail.com>
2016-09-09 12:41:56 -0400

    fixing node/layer and tests for s3

--- tests/testthat/test_sge_dgraph.R
+++ tests/testthat/test_sge_dgraph.R
@@ -31,38 +31,38 @@ context("load_results")
 expect_equal(unlist(load_results("t1.Rdata", fdir)), rep(c(2, 0), .reps))
 
 context("collect.dgraph")
-expect_equal(collect.dgraph(task = 1, dir = fdir), load_results("t1.Rdata", fdir))
+expect_equivalent(collect(o, task = 1, dir = fdir), load_results("t1.Rdata", fdir))
 
-res1 <- unlist(collect.dgraph(node = 1, dir=fdir))
+res1 <- unlist(collect(o, node = 1, dir=fdir))
 ans1 <- unlist(grid_apply(ff, a = 1:3, b = 1:3, .reps = .reps))
 expect_equal(res1, ans1)
 
-res2 <- unlist(collect.dgraph(node = 2, dir = fdir))
+res2 <- unlist(collect(o, node = 2, dir = fdir))
 ans2 <- unlist(grid_apply(ff, a = 4:5, b = 4:5, .reps = .reps))
 expect_equal(res2, ans2)
 
-res3 <- unlist(collect.dgraph(node = 3, dir = fdir))
+res3 <- unlist(collect(o, node = 3, dir = fdir))
 expect_equal(res3, hh(ans1))
 
-res4 <- unlist(collect.dgraph(node = 4, dir = fdir))
+res4 <- unlist(collect(o, node = 4, dir = fdir))
 expect_equal(res4, hh(ans2))
 
-res5 <- unlist(collect.dgraph(node = 5, dir = fdir))
+res5 <- unlist(collect(o, node = 5, dir = fdir))
 expect_equal(res5, gg(ans1))
 
-res6 <- unlist(collect.dgraph(node = 6, dir = fdir))
+res6 <- unlist(collect(o, node = 6, dir = fdir))
 expect_equal(res6, gg(ans2))
 
-res <- unlist(collect.dgraph(layer = 2, dir = fdir))
+res <- unlist(collect(o, layer = 2, dir = fdir))
 
 fin.ans <- c(hh(ans1, 1), hh(ans2, 1), gg(ans1, 1), gg(ans2, 1))
 expect_equal(res, fin.ans)
 
-res2 <- unlist(collect.dgraph(dir = fdir))
+res2 <- unlist(collect(o, dir = fdir))
 expect_equal(res, res2)
 
 context("tidy.dgraph")
-res.tidy <- collect.dgraph(dir = fdir) %>% tidy.dgraph(dir = fdir)
+res.tidy <- collect(o, dir = fdir) %>% tidy(., dir = fdir)
 expect_equal(res.tidy$value, fin.ans)
 
 
